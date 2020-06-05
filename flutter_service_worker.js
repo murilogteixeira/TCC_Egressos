5d2b0ffec7a55a5bf9e72588e58792544c5ba9a7
Murilo Teixeira <murilogteixeira@gmail.com>
2020-06-05 11:36:06 -0300

    Built web
    Branch: feature/criar-protótipo-funcional
    Commit: c645d5b71e50d629425f6da64bb484542bddc281 (dirty)
    
    package:peanut 3.5.1

---
+++ flutter_service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "index.html": "a923041e8c99b657a3079c1a30dbc436",
+"/": "a923041e8c99b657a3079c1a30dbc436",
+"main.dart.js": "9f5cf1da1e11b75f8304440eabd1f831",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "cf8c7addc9f1a85e6b1497274188348c",
+"assets/LICENSE": "7a71c60df68025674d073a05d2e5c8a8",
+"assets/AssetManifest.json": "8699a35905939653053a4b63373da2d8",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/progress_dialog/assets/double_ring_loading_io.gif": "e5b006904226dc824fdb6b8027f7d930",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
