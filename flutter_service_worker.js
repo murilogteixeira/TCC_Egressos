c4b96de11efe9fabfae09fff5815850c55387a50
Murilo Teixeira <murilogteixeira@gmail.com>
2020-06-05 13:43:00 -0300

    Built web
    Branch: feature/criar-protótipo-funcional
    Commit: 0abcd79614c715d584ab07d3d9633212e1a2af51 (dirty)
    
    package:peanut 3.5.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "a923041e8c99b657a3079c1a30dbc436",
 "/": "a923041e8c99b657a3079c1a30dbc436",
-"main.dart.js": "9f5cf1da1e11b75f8304440eabd1f831",
+"main.dart.js": "a9158c054c1573671507dcc72c493c18",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
