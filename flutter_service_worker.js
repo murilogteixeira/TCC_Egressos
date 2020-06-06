11854065dc6455bb4421e8e361605d108d604e74
Murilo Teixeira <murilogteixeira@gmail.com>
2020-06-06 16:38:06 -0300

    Built web
    Branch: feature/criar-base-tela-curriculo
    Commit: 3fe7e54a80ace2d52e72f2ba32e1bcdef4e543a0 (dirty)
    
    package:peanut 3.5.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "a923041e8c99b657a3079c1a30dbc436",
 "/": "a923041e8c99b657a3079c1a30dbc436",
-"main.dart.js": "713ec90fbafb66efc2e8dba92280528d",
+"main.dart.js": "20099f0591b6ee30af9fa24fb5f2a343",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
