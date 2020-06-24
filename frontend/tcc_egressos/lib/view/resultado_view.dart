d8c40abfcded2a908e343c37ab0d5f4e6c85a0ce
Ramon <dias.ramon07@hotmail.com>
2020-06-24 11:32:04 -0300

    Adicionar formatação na página de resultados

--- frontend/tcc_egressos/lib/view/resultado_view.dart
+++ frontend/tcc_egressos/lib/view/resultado_view.dart
@@ -36,9 +36,15 @@ class _ResultadoViewState extends State<ResultadoView> {
       var maxWidth = constraints.maxWidth;
 
       if (maxWidth >= 576) {
-        return Scaffold(appBar: _createAppBar(), body: _listContainer(lista));
+        return Scaffold(
+            appBar: _createAppBar(),
+            body: _listContainer(lista)
+        );
       }
-      return Scaffold(appBar: _createAppBar(), body: _listContainer(lista));
+      return Scaffold(
+          appBar: _createAppBar(),
+          body: _listContainer(lista)
+      );
     });
   }
 
@@ -68,9 +74,40 @@ class _ResultadoViewState extends State<ResultadoView> {
     //       );
     //     });
 
-    return ListEgressos(
-      list: lista,
-      sizeList: lista.length,
+    return Container(
+      padding: EdgeInsets.fromLTRB(200, 200, 200, 0),
+      color: Color(0xFFEAEDF2),
+      child: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20))
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.fromLTRB(50, 50, 0, 50),
+                    child: Text(
+                        '${lista.length} resultados encontrados',
+                        style: TextStyle(color: Colors.blue, fontSize: 20.0)
+                    )
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: ListEgressos(
+                    list: lista,
+                    sizeList: lista.length,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
