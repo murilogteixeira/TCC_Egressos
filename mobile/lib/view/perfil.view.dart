33ef739a8ebde88ec1c47786e520659c229f97d0
Gustavo Feliciano Figueiredo <gustavo.flcn@gmail.com>
2020-09-15 18:57:10 -0300

    Criar tela de perfil com dados mocados

--- mobile/lib/view/perfil.view.dart
+++ mobile/lib/view/perfil.view.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 class PerfilView extends StatefulWidget {
@@ -12,9 +14,182 @@ class _PerfilViewState extends State<PerfilView> {
       appBar: AppBar(
         title: Text('Perfil'),
       ),
-      body: Center(
-        child: Text('Vitoria Regia'),
-      ),
+      body: Padding(
+        padding: const EdgeInsets.only( 
+          left: 50,
+          right: 50,
+          top: 40
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: 
+              ClipRRect(
+                borderRadius: BorderRadius.circular(180),
+                child: Image.network(
+                  'https://picsum.photos/250?image=9',
+                  height: 250,
+                  width: 250,
+                )
+              )
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Vitoria Regia',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold, 
+                    fontSize: 28.0
+                  )
+                )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 2),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Nome de Citação:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold, 
+                    fontSize: 18.0
+                  )
+                )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:Text(
+                  'R. Vitoria',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0
+                  )
+                )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 2),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:Text(
+                  'Atuação:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold, 
+                    fontSize: 18.0
+                  )
+                )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:Text(
+                  'Docencia',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0
+                  )
+                )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 2),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:Text(
+                  'Cargo Atual:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold, 
+                    fontSize: 18.0
+                  )
+                )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:Text(
+                  'Professora Universitaria',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0
+                  )
+                )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 2),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:Text(
+                  'Conatos:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold, 
+                    fontSize: 18.0
+                  )
+                )
+              )
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 2),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:Text(
+                  'vitoriaregia@mail.com',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0
+                  )
+                )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 2),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:Text(
+                  'Linkedin@vitoria-regia',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0
+                  )
+                )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 2),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:Text(
+                  '@vitoriaRERE',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0
+                  )
+                )
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child:Text(
+                '(61)9 9999-9999',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0
+                )
+              )
+            ),
+          ],
+        ),
+      )
     );
   }
 }
