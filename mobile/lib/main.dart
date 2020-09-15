9872857e66af05632d04327b74f0aa8816880183
Murilo Teixeira <murilogteixeira@gmail.com>
2020-09-14 22:09:57 -0300

    Remover banner debug

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -2,3 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:mobile/view/home.view.dart';
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
