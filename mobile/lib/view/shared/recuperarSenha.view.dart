5afa4fd911ce6bc98d0caef3a7c5e9d64251dc63
Murilo Teixeira <murilogteixeira@gmail.com>
2020-10-29 20:03:03 -0300

    Adicionar botão para enviar solicitação de recuperar senha

--- mobile/lib/view/shared/recuperarSenha.view.dart
+++ mobile/lib/view/shared/recuperarSenha.view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class RecuperarSenha extends StatefulWidget {
-  static final route = "recuperarSenha";
+  static final route = "/recuperarSenha";
   @override
   _RecuperarSenhaState createState() => _RecuperarSenhaState();
 }
@@ -23,6 +23,25 @@ class _RecuperarSenhaState extends State<RecuperarSenha> {
     ),
   );
 
+  _solicitarRecuperacaoSenha(String email) {
+    print('Realizar solicitação ao backend para recuperar a senha');
+  }
+
+  _mostrarSnackBar(BuildContext context, String text) async {
+    const duration = 4000;
+    var snackBar = SnackBar(
+      content: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 18),
+      ),
+      duration: Duration(milliseconds: duration),
+    );
+    Scaffold.of(context).showSnackBar(snackBar);
+    await Future.delayed(Duration(milliseconds: duration + 500));
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     var formKey = GlobalKey<FormState>();
@@ -31,31 +50,66 @@ class _RecuperarSenhaState extends State<RecuperarSenha> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Esqueceu a Senha"),
+        backgroundColor: Color(0xFF547DD9),
       ),
-      body: Form(
-        key: formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Email",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
+      body: Builder(
+        builder: (context) => Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Email",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) return 'Insira o seu email';
-                  return null;
-                },
-                onSaved: (value) => inputEmail = value,
-                decoration: _inputDecoration,
-              ),
-            ],
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) return 'Insira o seu email';
+                    return null;
+                  },
+                  onSaved: (value) => inputEmail = value,
+                  decoration: _inputDecoration,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: 192,
+                    height: 50,
+                    child: RaisedButton(
+                      color: Color(0xFF30559F),
+                      child: Text(
+                        'Enviar',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 19,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      onPressed: () {
+                        if (formKey.currentState.validate()) {
+                          formKey.currentState.save();
+                          _solicitarRecuperacaoSenha(inputEmail);
+                          _mostrarSnackBar(
+                            context,
+                            'Confira o seu email!\n\nCaso esteja válido, em breve receberá as instruções de recuperação.',
+                          );
+                        }
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
