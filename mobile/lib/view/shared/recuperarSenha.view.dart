185c87c82fa7d6c4c1c8bb74867b459da1543515
Murilo Teixeira <murilogteixeira@gmail.com>
2020-11-03 18:56:36 -0300

    Implementar indicador de carregamento da web view

--- mobile/lib/view/shared/recuperarSenha.view.dart
+++ mobile/lib/view/shared/recuperarSenha.view.dart
@@ -8,60 +8,44 @@ class RecuperarSenha extends StatefulWidget {
 }
 
 class _RecuperarSenhaState extends State<RecuperarSenha> {
-  var _inputDecoration = InputDecoration(
-    contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-    focusedBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.all(Radius.circular(7)),
-      borderSide: BorderSide(color: Color(0xFF547DD9), width: 2.0),
-    ),
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.all(Radius.circular(7)),
-      borderSide: BorderSide(color: Color(0xFFDCDDE0), width: 2.0),
-    ),
-    errorBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.all(Radius.circular(7)),
-      borderSide: BorderSide(color: Colors.red, width: 2.0),
-    ),
-  );
+  String title, url;
+  bool isLoading = true;
+  final _key = UniqueKey();
 
-  var userLeftTheView = false;
-
-  _solicitarRecuperacaoSenha(String email) {
-    print(
-        'RecuperarSenhaView, linha 29: Realizar solicitação ao backend para recuperar a senha');
-  }
-
-  _mostrarSnackBar(BuildContext context, String text) async {
-    const duration = 4000;
-    var snackBar = SnackBar(
-      content: Text(
-        text,
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 18),
-      ),
-      duration: Duration(milliseconds: duration),
-    );
-    Scaffold.of(context).showSnackBar(snackBar);
-    await Future.delayed(Duration(milliseconds: duration + 500));
-    if (!userLeftTheView) Navigator.of(context).pop();
+  _RecuperarSenhaState() {
+    this.url = 'https://egressosbackend.herokuapp.com/accounts/password_reset/';
   }
 
   @override
   Widget build(BuildContext context) {
-    var recuperarSenhaURL =
-        'https://egressosbackend.herokuapp.com/accounts/password_reset/';
-
-    return WillPopScope(
-      onWillPop: () async {
-        userLeftTheView = true;
-        return true;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Esqueceu a Senha"),
-          backgroundColor: Color(0xFF547DD9),
-        ),
-        body: WebView(initialUrl: recuperarSenhaURL),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Esqueceu a Senha"),
+        backgroundColor: Color(0xFF547DD9),
+      ),
+      body: Stack(
+        children: [
+          WebView(
+            key: _key,
+            initialUrl: this.url,
+            javascriptMode: JavascriptMode.unrestricted,
+            onPageStarted: (url) {
+              setState(() {
+                isLoading = true;
+              });
+            },
+            onPageFinished: (finish) {
+              setState(() {
+                isLoading = false;
+              });
+            },
+          ),
+          isLoading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Stack(),
+        ],
       ),
     );
   }
