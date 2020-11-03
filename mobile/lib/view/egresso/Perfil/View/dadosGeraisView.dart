c8e35924831b71bf912e0290ee1b172512d3140e
Gustavo Feliciano Figueiredo <gustavo.flcn@gmail.com>
2020-11-03 11:26:19 -0300

    Fazer o  layout na aba de dados gerais

--- mobile/lib/view/egresso/Perfil/View/dadosGeraisView.dart
+++ mobile/lib/view/egresso/Perfil/View/dadosGeraisView.dart
@@ -5,115 +5,216 @@ class DadosGeraisView extends StatefulWidget {
   const DadosGeraisView({Key key, this.controller}) : super(key: key);
 
   final PerfilController controller;
-
   @override
   _DadosGeraisViewState createState() => _DadosGeraisViewState();
 }
 
 class _DadosGeraisViewState extends State<DadosGeraisView> {
+  Widget pointIndicator = new Container(
+    width: 10,
+    height: 10,
+    decoration:
+        new BoxDecoration(color: Color(0xFFB4AFED), shape: BoxShape.circle),
+  );
+  Widget horizontalSeparator = new Container(
+    width: 363.23,
+    height: 1,
+    decoration:
+        new BoxDecoration(color: Color(0xFFD1D1D1), shape: BoxShape.rectangle),
+  );
+  Widget verticalSeparator = new Container(
+    width: 2,
+    height: 32.25,
+    decoration:
+        new BoxDecoration(color: Color(0xFFEBEAF2), shape: BoxShape.rectangle),
+  );
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        child: Column(
-          children: <Widget>[
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              pointIndicator,
               Padding(
-              padding: EdgeInsets.only(bottom: 2),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('ID Latters:',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 18.0))),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                      widget.controller.egresso.idLattes,
-                      style: TextStyle(color: Colors.black, fontSize: 20.0))),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 2),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Nome de Citação:',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 18.0))),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                      widget.controller.egresso.nomeCitacoes,
-                      style: TextStyle(color: Colors.black, fontSize: 20.0))),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 2),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Atuação:',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 18.0))),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Docencia',
-                      style: TextStyle(color: Colors.black, fontSize: 20.0))),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 2),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Cargo Atual:',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 18.0))),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Professor Universitario',
-                      style: TextStyle(color: Colors.black, fontSize: 20.0))),
-            ),
-            Padding(
-                padding: EdgeInsets.only(bottom: 2),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text('Conatos:',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18.0)))),
-            Padding(
-              padding: EdgeInsets.only(bottom: 2),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(widget.controller.egresso.email,
-                      style: TextStyle(color: Colors.black, fontSize: 20.0))),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 2),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Linkedin@mario-braga',
-                      style: TextStyle(color: Colors.black, fontSize: 20.0))),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 2),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('@mariobragaucb',
-                      style: TextStyle(color: Colors.black, fontSize: 20.0))),
-            ),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text(widget.controller.egresso.celular ?? '',
-                    style: TextStyle(color: Colors.black, fontSize: 20.0))),
-          ],
+                padding: const EdgeInsets.fromLTRB(24, 8, 8, 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 9),
+                      child: Text('ID Latters:',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Color(0xFF547DD9), fontWeight: FontWeight.bold, fontSize: 18.0)),
+                    ),
+                    Text(widget.controller.egresso.idLattes,
+                        style: TextStyle(color: Color(0xFF4A4A4A), fontSize: 20.0))
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 27, 40, 27),
+          child: horizontalSeparator,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              pointIndicator,
+              Padding(
+                padding: const EdgeInsets.fromLTRB(24, 8, 8, 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 9),
+                      child: Text('Nome de Citação:',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Color(0xFF547DD9), fontWeight: FontWeight.bold, fontSize: 18.0)),
+                    ),
+                    Text(widget.controller.egresso.nomeCitacoes,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(color: Color(0xFF4A4A4A), fontSize: 20.0)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 27, 40, 27),
+          child: horizontalSeparator,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              pointIndicator,
+              Padding(
+                padding: const EdgeInsets.fromLTRB(24, 8, 8, 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 9),
+                      child: Text('Atuação:',
+                          style: TextStyle(
+                              color: Color(0xFF547DD9), fontWeight: FontWeight.bold, fontSize: 18.0)),
+                    ),
+                    Text('Docencia',
+                        style: TextStyle(color: Color(0xFF4A4A4A), fontSize: 20.0))
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 27, 40, 27),
+          child: horizontalSeparator,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              pointIndicator,
+              Padding(
+                padding: const EdgeInsets.fromLTRB(24, 8, 8, 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 9),
+                      child: Text('Cargo Atual:',
+                          style: TextStyle(
+                              color: Color(0xFF547DD9), fontWeight: FontWeight.bold, fontSize: 18.0)),
+                    ),
+                    Text('Professor Universitario',
+                        style: TextStyle(color: Color(0xFF4A4A4A), fontSize: 20.0)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 27, 40, 27),
+          child: horizontalSeparator,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(34, 8, 8, 9),
+                child: Text('Conatos:',
+                    style:
+                        TextStyle(color: Color(0xFF547DD9),fontWeight: FontWeight.bold, fontSize: 18.0)),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  pointIndicator,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(24, 8, 8, 8),
+                    child: Text(widget.controller.egresso.email,
+                        style: TextStyle(color: Color(0xFF4A4A4A), fontSize: 20.0)),
+                  ),
+                ],
+              ),
+              // Padding(
+              //   padding: const EdgeInsets.only(left: 4),
+              //   child: verticalSeparator,
+              // ),
+              // Row(
+              //   children: [
+              //     pointIndicator,
+              //     Padding(
+              //       padding: const EdgeInsets.fromLTRB(24, 8, 8, 8),
+              //       child: Text('Linkedin@mario-braga',
+              //           style: TextStyle(color: Color(0xFF4A4A4A), fontSize: 20.0)),
+              //     ),
+              //   ],
+              // ),
+              // Padding(
+              //   padding: const EdgeInsets.only(left: 4),
+              //   child: verticalSeparator,
+              // ),
+              // Row(
+              //   children: [
+              //     pointIndicator,
+              //     Padding(
+              //       padding: const EdgeInsets.fromLTRB(24, 8, 8, 8),
+              //       child: Text('@mariobragaucb',
+              //           style: TextStyle(color: Color(0xFF4A4A4A), fontSize: 20.0)),
+              //     ),
+              //   ],
+              // ),
+              Padding(
+                padding: const EdgeInsets.only(left: 4),
+                child: verticalSeparator,
+              ),
+              Row(
+                children: [
+                  pointIndicator,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(24, 8, 8, 8),
+                    child: Text(widget.controller.egresso.celular ?? '',
+                        style: TextStyle(color: Color(0xFF4A4A4A), fontSize: 20.0)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- mobile/lib/view/egresso/Perfil/perfil.view.dart
+++ mobile/lib/view/egresso/Perfil/perfil.view.dart
@@ -45,115 +45,113 @@ class _PerfilViewState extends State<PerfilView> {
             ? Center(
                 child: CircularProgressIndicator(),
               )
-            : Padding(
-                padding: const EdgeInsets.only(left: 0, right: 0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.085,
-                      ),
-                      Padding(
-                          padding: EdgeInsets.only(bottom: 10, top: 40),
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.circular(180),
-                              child: Image.network(
-                                'https://www.pngitem.com/pimgs/m/421-4212617_person-placeholder-image-transparent-hd-png-download.png',
-                                height: 90,
-                                width: 90,
-                              ))),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 10),
-                        child: Align(
-                            alignment: Alignment.center,
-                            child: Text(_controller.egresso.nome,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 18.0,
-                                  color: Color(0xFF547DD9),
-                                ))),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 40),
-                        child: Align(
+            : Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.01,
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(bottom: 10, top: 40),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(180),
+                            child: Image.network(
+                              'https://www.pngitem.com/pimgs/m/421-4212617_person-placeholder-image-transparent-hd-png-download.png',
+                              height: 90,
+                              width: 90,
+                            ))),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Align(
                           alignment: Alignment.center,
-                          child: Container(
-                            alignment: Alignment.center,
-                            width: 109,
-                            height: 20,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(4),
-                                color: Color(0xFF70D169)),
-                            child: Text(
-                              "Regular",
+                          child: Text(_controller.egresso.nome,
                               style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 10.0,
-                                color: Colors.white,
-                              ),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18.0,
+                                color: Color(0xFF547DD9),
+                              ))),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 40),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 109,
+                          height: 20,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4),
+                              color: Color(0xFF70D169)),
+                          child: Text(
+                            "Regular",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 10.0,
+                              color: Colors.white,
                             ),
                           ),
                         ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 40),
-                        child: Container(
-                          margin: EdgeInsets.symmetric(vertical: 20.0),
-                          height: 50.0,
-                          color: Colors.white,
-                          width: (MediaQuery.of(context).size.width),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Container(
-                                  width: 130.0,
-                                  color: Colors.white,
-                                  child: FlatButton(
-                                      onPressed: () {
-                                        _controller.setInformacoesEgresso(
-                                            dadosGeraisView);
-                                      },
-                                      child: Text(
-                                        "Dados Gerais",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 13.0,
-                                          color: Color(0xFF547DD9),
-                                        ),
-                                      )),
-                                ),
-                                Container(
-                                  width: 130.0,
-                                  color: Colors.white,
-                                  child: FlatButton(
-                                      onPressed: () {
-                                        _controller.setInformacoesEgresso(
-                                            producoesEgressoView);
-                                      },
-                                      child: Text(
-                                        "Produções",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 13.0,
-                                          color: Color(0xFF547DD9),
-                                        ),
-                                      )),
-                                ),
-                              ]),
-                        ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 40),
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 20.0),
+                        height: 50.0,
+                        color: Colors.white,
+                        width: (MediaQuery.of(context).size.width),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                width: 130.0,
+                                color: Colors.white,
+                                child: FlatButton(
+                                    onPressed: () {
+                                      _controller.setInformacoesEgresso(
+                                          dadosGeraisView);
+                                    },
+                                    child: Text(
+                                      "Dados Gerais",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 13.0,
+                                        color: Color(0xFF547DD9),
+                                      ),
+                                    )),
+                              ),
+                              Container(
+                                width: 130.0,
+                                color: Colors.white,
+                                child: FlatButton(
+                                    onPressed: () {
+                                      _controller.setInformacoesEgresso(
+                                          producoesEgressoView);
+                                    },
+                                    child: Text(
+                                      "Produções",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 13.0,
+                                        color: Color(0xFF547DD9),
+                                      ),
+                                    )),
+                              ),
+                            ]),
                       ),
-                      Observer(
-                        builder: (_) => Padding(
-                          padding: EdgeInsets.symmetric(vertical: 40),
-                          child: Align(
-                              alignment: Alignment.bottomCenter,
-                              child: _controller.informacoesEgresso),
-                        ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40.0),
+                      child: Observer(
+                        builder: (_) => _controller.informacoesEgresso,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
+            ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.edit),
