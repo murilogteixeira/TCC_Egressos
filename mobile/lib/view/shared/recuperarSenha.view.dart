d0944421582d916ced943fdfb6bf07f9034bf36c
Murilo Teixeira <murilogteixeira@gmail.com>
2020-11-03 18:20:06 -0300

    Adicionar recuperação de senha

--- mobile/lib/view/shared/recuperarSenha.view.dart
+++ mobile/lib/view/shared/recuperarSenha.view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:mobile/controller/recuperarSenha.controller.dart';
-import 'package:mobile/main.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class RecuperarSenha extends StatefulWidget {
   static final route = "/recuperarSenha";
@@ -50,10 +48,8 @@ class _RecuperarSenhaState extends State<RecuperarSenha> {
 
   @override
   Widget build(BuildContext context) {
-    var _controller = RecuperarSenhaController();
-
-    var formKey = GlobalKey<FormState>();
-    String inputEmail;
+    var recuperarSenhaURL =
+        'https://egressosbackend.herokuapp.com/accounts/password_reset/';
 
     return WillPopScope(
       onWillPop: () async {
@@ -65,84 +61,7 @@ class _RecuperarSenhaState extends State<RecuperarSenha> {
           title: Text("Esqueceu a Senha"),
           backgroundColor: Color(0xFF547DD9),
         ),
-        body: Builder(
-          builder: (context) => Form(
-            key: formKey,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Email",
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Observer(
-                    builder: (context) => TextFormField(
-                      enabled: !_controller.isLoading,
-                      decoration: _inputDecoration,
-                      validator: (value) {
-                        const msgErro = 'Email inválido';
-                        if (value.isEmpty) return msgErro;
-
-                        var stringSeparatedByAtSign = value.split('@');
-                        var domainSeparatedByDot =
-                            stringSeparatedByAtSign[1].split('.');
-
-                        if (stringSeparatedByAtSign.length != 2) return msgErro;
-                        if (stringSeparatedByAtSign[0].length <= 0 ||
-                            stringSeparatedByAtSign[1].length <= 0)
-                          return msgErro;
-
-                        if (domainSeparatedByDot.length <= 1) return msgErro;
-
-                        return null;
-                      },
-                      onSaved: (value) => inputEmail = value,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Center(
-                    child: SizedBox(
-                      width: 192,
-                      height: 50,
-                      child: RaisedButton(
-                        color: mainColor,
-                        child: Text(
-                          'Enviar',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 19,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                        onPressed: () {
-                          if (formKey.currentState.validate()) {
-                            formKey.currentState.save();
-                            _controller.setIsLoading(true);
-                            _solicitarRecuperacaoSenha(inputEmail);
-                            _mostrarSnackBar(
-                              context,
-                              'Confira o seu email!\n\nCaso esteja válido, em breve receberá as instruções de recuperação.',
-                            );
-                          }
-                        },
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25)),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+        body: WebView(initialUrl: recuperarSenhaURL),
       ),
     );
   }
